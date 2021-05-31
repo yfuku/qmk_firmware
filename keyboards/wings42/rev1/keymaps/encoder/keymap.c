5e1916a543573e5f2666d70335001ea86454ab39
yfuku <fukumoto@gmail.com>
2021-05-31 18:05:30 +0900

    fix encoder for wings42

--- keyboards/wings42/rev1/keymaps/encoder/keymap.c
+++ keyboards/wings42/rev1/keymaps/encoder/keymap.c
@@ -1,244 +1,109 @@
+/* Copyright 2021 yfuku
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 #include QMK_KEYBOARD_H
-#ifdef PROTOCOL_LUFA
-  #include "lufa.h"
-  #include "split_util.h"
-#endif
-#ifdef SSD1306OLED
-  #include "ssd1306.h"
-#endif
-
-extern keymap_config_t keymap_config;
-
-extern uint8_t is_master;
-
-// Each layer gets a name for readability, which is then used in the keymap matrix below.
-// The underscores don't mean anything - you can have a layer called STUFF or any other name.
-// Layer names don't all need to be of the same length, obviously, and you can also skip them
-// entirely and just use numbers.
-enum custom_keycodes {
-  QWERTY = SAFE_RANGE,
-  LOWER,
-  RAISE
-};
 
 enum layer_number {
     _QWERTY = 0,
     _RAISE,
     _LOWER,
+    _ADJUST,
 };
 
-// common
-#define KC_ KC_TRNS
-#define KC_XXXX KC_NO
-#define KC_RST RESET
-#define KC_VD KC__VOLDOWN
-#define KC_VU KC__VOLUP
-
-// layer
+#define KC_G_TAB LGUI_T(KC_TAB)
+#define KC_G_BS LGUI_T(KC_BSPC)
 #define KC_L_SPC LT(_LOWER, KC_SPC)
 #define KC_R_ENT LT(_RAISE, KC_ENT)
-
-// shift_t
-#define KC_S_EN LSFT_T(KC_LANG2)
 #define KC_S_JA LSFT_T(KC_LANG1)
-
-// cmd_t
-#define KC_G_F LCMD_T(KC_F)
-#define KC_G_J RCMD_T(KC_J)
-
-// ctl_t
-#define KC_C_G LCTL_T(KC_G)
-#define KC_C_H RCTL_T(KC_H)
-
-// alt_t
-#define KC_A_D ALT_T(KC_D)
-#define KC_A_K ALT_T(KC_K)
-
-// cmd+shift_t
-#define KC_GS_S SCMD_T(KC_S)
-#define KC_GS_L SCMD_T(KC_L)
-
-//
-#define KC_MISS C(KC_UP)
-
-#define TAPPING_LAYER_TERM 230
-uint16_t get_tapping_term(uint16_t keycode, keyrecord_t *record) {
-  switch (keycode) {
-    case KC_GS_S:
-      return TAPPING_LAYER_TERM;
-    case KC_GS_L:
-      return TAPPING_LAYER_TERM;
-    case KC_A_D:
-      return TAPPING_LAYER_TERM;
-    case KC_A_K:
-      return TAPPING_LAYER_TERM;
-    default:
-      return TAPPING_TERM;
-  }
-}
+#define KC_S_EN LSFT_T(KC_LANG2)
+#define KC_A_DEL ALT_T(KC_DEL)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    [_QWERTY] = LAYOUT(
+    //,--------+--------+---------+--------+---------+--------.   ,--------+---------+--------+---------+--------+--------.
+       KC_ESC , KC_Q   , KC_W    , KC_E   , KC_R    , KC_T   ,     KC_Y   , KC_U    , KC_I   , KC_O    , KC_P   , KC_MINS,
+    //|--------+--------+---------+--------+---------+--------|   |--------+---------+--------+---------+--------+--------|
+      KC_G_TAB, KC_A   , KC_S    , KC_D   , KC_F    , KC_G   ,     KC_H   , KC_J    , KC_K   , KC_L    , KC_SCLN, KC_G_BS ,
+    //|--------+--------+---------+--------+---------+--------|   |--------+---------+--------+---------+--------+--------|
+       KC_LCTL, KC_Z   , KC_X    , KC_C   , KC_V    , KC_B   ,     KC_N   , KC_M    , KC_COMM, KC_DOT  , KC_SLSH, KC_RCTL,
+    //`--------+--------+---------+--------+---------+--------/   \--------+---------+--------+---------+--------+--------'
+                                  KC_A_DEL, KC_S_EN ,KC_L_SPC,    KC_R_ENT, KC_S_JA , KC_A_DEL
+    //                           `+--------+---------+--------'   `--------+---------+--------+'
+    ),
 
-  // M_ = LCMD_T(
-  // A_ = ALT_T(
-  // C_ = LCTL_T(
-  // MS_ = SMD_T(
-  // R_ = LT(_RAISE
-  // L_ = LT(_LOWER
+    [_RAISE] = LAYOUT( \
+    //,--------+--------+--------+--------+--------+--------.   ,--------+--------+--------+--------+--------+--------.
+       _______, KC_BSLS, KC_CIRC, KC_EXLM, KC_AMPR, KC_PIPE,     KC_AT  , KC_EQL , KC_PLUS, KC_ASTR, KC_PERC, KC_MINS,
+    //|--------+--------+--------+--------+--------+--------|   |--------+--------+--------+--------+--------+--------|
+       KC_LPRN, KC_HASH, KC_DLR , KC_DQT , KC_QUOT, KC_TILD,     KC_LEFT, KC_DOWN,  KC_UP , KC_RGHT, KC_GRV , KC_RPRN,
+    //|--------+--------+--------+--------+--------+--------|   |--------+--------+--------+--------+--------+--------|
+       _______, _______, _______, _______, KC_LCBR, KC_LBRC,     KC_RBRC, KC_RCBR, _______, _______, _______, _______,
+    //`--------+--------+--------+--------+--------+--------/   \--------+--------+--------+--------+--------+--------'
+                                  _______, _______, _______,     _______, _______, RESET
+    //                          `+--------+--------+--------'   `--------+---------+--------+'
+    ),
 
-  [_QWERTY] = LAYOUT_kc( \
-  //,----+----+----+----+----+----.        ,----+----+----+----+----+----.
-      ESC, Q  , W  , E  , R  , T  ,          Y  , U  , I  , O  , P  ,MINS,
-  //|----+----+----+----+----+----|        |----+----+----+----+----+----|
-      TAB, A  ,GS_S,A_D ,G_F ,C_G ,         C_H ,G_J ,A_K ,GS_L,SCLN,ESC ,
-  //|----+----+----+----+----+----+        |----+----+----+----+----+----|
-     LEFT, Z  , X  , C  , V  , B  ,          N  , M  ,COMM,DOT ,SLSH,RGHT,
-  //`----+----+----+----+----+----/        \----+----+----+----+----+----'
-                    DOWN,S_EN,L_SPC,       R_ENT,S_JA,UP
-  //          `----+----+----+----'        `----+----+----+----'
-  ),
+    [_LOWER] = LAYOUT( \
+    //,--------+--------+--------+--------+--------+--------.   ,--------+--------+--------+--------+--------+--------.
+       KC_F1  , KC_F2  , KC_F3  , KC_F4  , KC_F5  , KC_F6  ,     _______, KC_EQL , KC_PLUS, KC_ASTR, KC_PERC, KC_MINS,
+    //|--------+--------+--------+--------+--------+--------|   |--------+--------+--------+--------+--------+--------|
+       _______, KC_1   , KC_2   , KC_3   , KC_4   , KC_5   ,     KC_6   , KC_7   , KC_8   , KC_9   , KC_0   , _______,
+    //|--------+--------+--------+--------+--------+--------|   |--------+--------+--------+--------+--------+--------|
+       KC_F7  , KC_F8  , KC_F9  , KC_F10 , KC_F11 , KC_F12 ,     _______, _______, KC_COMM, KC_DOT , KC_SLSH, _______,
+    //`--------+--------+--------+--------+--------+--------/   \--------+--------+--------+--------+--------+--------'
+                                  RESET  , _______, _______,     _______, _______, _______
+    //                          `+--------+--------+--------'   `--------+--------+--------+'
+    ),
 
-  //   \ ! & |      = + * %
-  //   # ` " ' ~  ← ↓ ↑ → $
-  //       { [ (  ) ] } @ ^
-
-  [_RAISE] = LAYOUT_kc( \
-  //,----+----+----+----+----+----.        ,----+----+----+----+----+----.
-         ,BSLS,EXLM,AMPR,PIPE,XXXX,         XXXX,EQL ,PLUS,ASTR,PERC,MINS,
-  //|----+----+----+----+----+----|        |----+----+----+----+----+----|
-         ,HASH,GRV ,DQT ,QUOT,TILD,         LEFT,DOWN, UP ,RGHT,DLR ,    ,
-  //|----+----+----+----+----+----|        |----+----+----+----+----+----|
-         ,    ,    ,LCBR,LBRC,LPRN,         RPRN,RBRC,RCBR,AT  ,CIRC,    ,
-  //`----+----+----+----+----+----/        \----+----+----+----+----+----'
-                        ,    ,BSPC         ,    ,    ,RST
-  //          `----+----+----+----'        `----+----+----+----'
-  ),
-
-  [_LOWER] = LAYOUT_kc( \
-  //,----+----+----+----+----+----.        ,----+----+----+----+----+----.
-         ,    ,    ,    ,    ,XXXX,         XXXX,EQL ,PLUS,ASTR,PERC,MINS,
-  //|----+----+----+----+----+----|        |----+----+----+----+----+----|
-         , 1  , 2  , 3  , 4  , 5  ,          6  , 7  , 8  , 9  , 0  ,    ,
-  //|----+----+----+----+----+----|        |----+----+----+----+----+----|
-         ,    ,    ,    ,    ,    ,             ,    ,COMM,DOT ,SLSH,    ,
-  //`----+----+----+----+----+----/        \----+----+----+----+----+----'
-                    RST ,    ,    ,        DEL ,    ,
-  //          `----+----+----+----'        `----+----+----+----'
-  ),
-
+    [_ADJUST] = LAYOUT( \
+    //,--------+--------+--------+--------+--------+--------.   ,--------+--------+--------+--------+--------+--------.
+       _______, _______, _______, _______, _______, _______,     _______, _______, _______, _______, _______, _______,
+    //|--------+--------+--------+--------+--------+--------|   |--------+--------+--------+--------+--------+--------|
+       _______, _______, _______, _______, _______, _______,     _______, _______, _______, _______, _______, _______,
+    //|--------+--------+--------+--------+--------+--------|   |--------+--------+--------+--------+--------+--------|
+       _______, _______, _______, _______, _______, _______,     _______, _______, _______, _______, _______, _______,
+    //`--------+--------+--------+--------+--------+--------/   \--------+--------+--------+--------+--------+--------'
+                                  _______, _______, _______,     _______, _______, _______
+    //                          `+--------+--------+--------'   `--------+--------+--------+'
+    ),
 };
 
-
-
-void matrix_init_user(void) {
-  //SSD1306 OLED init, make sure to add #define SSD1306OLED in config.h
-  #ifdef SSD1306OLED
-    iota_gfx_init(!has_usb());   // turns on the display
-  #endif
-}
-
-//SSD1306 OLED update loop, make sure to add #define SSD1306OLED in config.h
-#ifdef SSD1306OLED
-
-// When add source files to SRC in rules.mk, you can use functions.
-const char *read_layer_state(void);
-const char *read_logo(void);
-void set_keylog(uint16_t keycode, keyrecord_t *record);
-const char *read_keylog(void);
-const char *read_keylogs(void);
-
-// const char *read_mode_icon(bool swap);
-// const char *read_host_led_state(void);
-// void set_timelog(void);
-// const char *read_timelog(void);
-
-void matrix_scan_user(void) {
-  iota_gfx_task();
-}
-
-void matrix_render_user(struct CharacterMatrix *matrix) {
-  if (is_master) {
-    // If you want to change the display of OLED, you need to change here
-    matrix_write_ln(matrix, read_layer_state());
-    matrix_write_ln(matrix, read_keylog());
-    matrix_write_ln(matrix, read_keylogs());
-    //matrix_write_ln(matrix, read_mode_icon(keymap_config.swap_lalt_lgui));
-    //matrix_write_ln(matrix, read_host_led_state());
-    //matrix_write_ln(matrix, read_timelog());
-  } else {
-    matrix_write(matrix, read_logo());
-  }
-}
-
-void matrix_update(struct CharacterMatrix *dest, const struct CharacterMatrix *source) {
-  if (memcmp(dest->display, source->display, sizeof(dest->display))) {
-    memcpy(dest->display, source->display, sizeof(dest->display));
-    dest->dirty = true;
-  }
-}
-
-void iota_gfx_task_user(void) {
-  struct CharacterMatrix matrix;
-  matrix_clear(&matrix);
-  matrix_render_user(&matrix);
-  matrix_update(&display, &matrix);
-}
-#endif//SSD1306OLED
-
-bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-  if (record->event.pressed) {
-#ifdef SSD1306OLED
-    set_keylog(keycode, record);
-#endif
-    // set_timelog();
-  }
-
-  switch (keycode) {
-    case QWERTY:
-      if (record->event.pressed) {
-        set_single_persistent_default_layer(_QWERTY);
-      }
-      return false;
-      break;
-    case LOWER:
-      if (record->event.pressed) {
-        layer_on(_LOWER);
-      } else {
-        layer_off(_LOWER);
-      }
-      return false;
-      break;
-    case RAISE:
-      if (record->event.pressed) {
-        layer_on(_RAISE);
-      } else {
-        layer_off(_RAISE);
-      }
-      return false;
-      break;
-  }
-  return true;
-}
-
 void encoder_update_user(uint8_t index, bool clockwise) {
-    if (index == 0) { /* First encoder */
+    if (index == 0) {        // left ↓
         if (clockwise) {
-            tap_code(KC_WH_U);
+            tap_code(KC_1);
         } else {
-            tap_code(KC_WH_D);
+            tap_code(KC_2);
         }
-    } else if (index == 1) { /* Second encoder */
-        if (clockwise) {
-            tap_code(KC_F11);
-        } else {
-            tap_code(KC_F12);
-        }
-    } else {
+    } else if (index == 1) { // left ↑
         if (clockwise) {
             tap_code(KC_3);
         } else {
             tap_code(KC_4);
         }
+    } else if (index == 2) { // right ↓
+        if (clockwise) {
+            tap_code(KC_5);
+        } else {
+            tap_code(KC_6);
+        }
+    } else if (index == 3) { // right ↑
+        if (clockwise) {
+            tap_code(KC_7);
+        } else {
+            tap_code(KC_8);
+        }
     }
 }
-
