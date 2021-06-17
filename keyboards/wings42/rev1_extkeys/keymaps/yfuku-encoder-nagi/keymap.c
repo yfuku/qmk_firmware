6793b0fba907d1b1895bfd5191604265e4a7fdb1
yfuku <fukumoto@gmail.com>
2021-06-17 11:04:23 +0900

    fix

--- keyboards/wings42/rev1_extkeys/keymaps/yfuku-encoder-nagi/keymap.c
+++ keyboards/wings42/rev1_extkeys/keymaps/yfuku-encoder-nagi/keymap.c
@@ -104,8 +104,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     _______,NG_Q   ,NG_W   ,NG_E   ,NG_R   ,NG_T   ,                NG_Y   ,NG_U   ,NG_I   ,NG_O   ,NG_P   ,_______, \
     _______,NG_A   ,NG_S   ,NG_D   ,NG_F   ,NG_G   ,_______,_______,NG_H   ,NG_J   ,NG_K   ,NG_L   ,NG_SCLN,_______, \
     _______,NG_Z   ,NG_X   ,NG_C   ,NG_V   ,NG_B   ,_______,_______,NG_N   ,NG_M   ,NG_COMM,NG_DOT ,NG_SLSH,_______, \
-                            _______,NG_SHFT,_______,                _______,NG_SHFT2,_______
-                          //_______,_______,NG_SHFT,                NG_SHFT2,_______,_______
+                          //  _______,NG_SHFT,_______,                _______,NG_SHFT2,_______
+                          _______,_______,NG_SHFT,                NG_SHFT2,_______,_______
   ),
 // 薙刀式
 
