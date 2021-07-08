7c5f1626c1aacc952beb169f5f3f201ffc94ce69
yfuku <fukumoto@gmail.com>
2021-07-08 23:42:02 +0900

    add wings42 rev2

--- keyboards/wings42/rev2/rev2.h
+++ keyboards/wings42/rev2/rev2.h
@@ -26,23 +26,19 @@
  * The second converts the arguments into a two-dimensional array which
  * represents the switch matrix.
  */
-#define LAYOUT( \
+#define LAYOUT_split_3x6_3_2( \
     L00, L01, L02, L03, L04, L05,           R00, R01, R02, R03, R04, R05, \
-    L10, L11, L12, L13, L14, L15, E01, E03, R10, R11, R12, R13, R14, R15, \
-    L20, L21, L22, L23, L24, L25, E02, E04, R20, R21, R22, R23, R24, R25, \
-                        L30, L31, L32, R30, R31, R32 \
+    L10, L11, L12, L13, L14, L15, L31, R33, R10, R11, R12, R13, R14, R15, \
+    L20, L21, L22, L23, L24, L25, L32, R34, R20, R21, R22, R23, R24, R25, \
+                        L33, L34, L35, R30, R31, R32 \
     ) \
     { \
-        { L00, L01, L02, L03, L04, L05, KC_NO, KC_NO }, \
-        { L10, L11, L12, L13, L14, L15, KC_NO, KC_NO }, \
-        { L20, L21, L22, L23, L24, L25, KC_NO, KC_NO }, \
-        { KC_NO, KC_NO, KC_NO, L30, L31, L32, KC_NO, KC_NO }, \
-        { KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, E02, KC_NO }, \
-        { KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, E01 }, \
-        { R05, R04, R03, R02, R01, R00, KC_NO, KC_NO }, \
-        { R15, R14, R13, R12, R11, R10, KC_NO, KC_NO }, \
-        { R25, R24, R23, R22, R21, R20, KC_NO, KC_NO }, \
-        { KC_NO, KC_NO, KC_NO, R32, R31, R30, KC_NO, KC_NO }, \
-        { KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, E04, KC_NO}, \
-        { KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, E03} \
+        { L00, L01, L02, L03, L04, L05 }, \
+        { L10, L11, L12, L13, L14, L15 }, \
+        { L20, L21, L22, L23, L24, L25 }, \
+        { KC_NO, L31, L32, L33, L34, L35 }, \
+        { R05, R04, R03, R02, R01, R00 }, \
+        { R15, R14, R13, R12, R11, R10 }, \
+        { R25, R24, R23, R22, R21, R20 }, \
+        { KC_NO, R34, R33, R32, R31, R30 } \
     }
